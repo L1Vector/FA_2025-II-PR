5883e7b1c81e87b29bd0b308a4881d194282fe11
Vector <ronaldvector210@gmail.com>
2025-09-02 15:30:06 -0500

    subiendo ejercicio1-SEM1

---
+++ SEMANA_1_C#/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SEMANA_1_C_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ejer1();
+            Console.ReadKey();
+        }
+        static void ejer1()
+        {
+            //Declaraciones
+            string nombre, carrera;
+            
+            Console.Write("Ingresa tu nombre: ");
+            nombre= Console.ReadLine();
+            
+            Console.Write("Ingresa tu carrera: ");
+            carrera= Console.ReadLine();
+
+            //Mensaje impreso
+            Console.WriteLine($"\n{nombre}, Bienvenido al curso de Fundamentos de Algoritmos de la carrera {carrera}");
+           
+
+        }
+        static void ejer2()
+        {
+
+        }
+        static void ejer3()
+        {
+
+        }
+        static void ejer4()
+        {
+
+        }
+        static void ejer5()
+        {
+
+        }
+        static void ejer6()
+        {
+
+        }
+
+    }
+}
---
+++ SEMANA_1_PYTHON/SEMANA_1_PYTHON.py
@@ -0,0 +1,8 @@
+def ejer1():
+    #Declaracion y escritura
+    nombre = input("Ingrese su nombre: ")
+    carrera = input("Ingrese su carrera: ")
+
+    print(f"\n{nombre}, bienvenido a Fa de {carrera}") 
+
+ejer1()
